657d8e905b7759546e71a74df45fcc4f49ff763e
crazy-zerg <63568127+crazy-zerg@users.noreply.github.com>
2020-04-20 10:58:59 +0800

    Add files via upload

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,14 +1,14 @@
 import {
   Cart
 } from '../cart/cart-model.js';
-// import {
-//   Order
-// } from '../order/order-model.js';
+import {
+  Order
+} from '../order/order-model.js';
 import {
   Address
 } from '../../utils/address.js';
 var cart = new Cart();
-// var order = new Order();
+var order = new Order();
 var address = new Address();
 
 Page({
@@ -17,6 +17,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    id: -1
   },
 
   /**
@@ -85,5 +86,113 @@ Page({
       addressInfo: addressInfo
     });
   },
-  
+  //支付下单接口
+  pay: function() {
+    if (!this.data.addressInfo) {
+      this.showTips('下单提示', '没有地址我怎么给你发货嘛');
+      return;
+    }
+    if (this.data.orderStatus == 0) {
+      this._firstTimePay();
+    } else {
+      this._oneMoresTimePay();
+    }
+  },
+  /*第一次支付*/
+  _firstTimePay: function() {
+    var orderInfo = [],
+      procuctInfo = this.data.productsArr,
+      order = new Order();
+    for (let i = 0; i < procuctInfo.length; i++) {
+      orderInfo.push({
+        product_id: procuctInfo[i].id,
+        count: procuctInfo[i].counts
+      });
+    }
+
+    var that = this;
+    //支付分两步，第一步是生成订单号，然后根据订单号支付
+    order.doOrder(orderInfo, (data) => {
+      //订单生成成功
+      if (data.pass) {
+        //更新订单状态
+        var id = data.order_id;
+        that.data.id = id;
+        //that.data.fromCartFlag = false;
+
+        //开始支付
+        that._execPay(id);
+      } else {
+        that._orderFail(data); // 下单失败
+      }
+    });
+  },
+  /*
+   *下单失败
+   * params:
+   * data - {obj} 订单结果信息
+   * */
+  _orderFail: function(data) {
+    var nameArr = [],
+      name = '',
+      str = '',
+      pArr = data.pStatusArray;
+    for (let i = 0; i < pArr.length; i++) {
+      if (!pArr[i].haveStock) {
+        name = pArr[i].name;
+        if (name.length > 15) {
+          name = name.substr(0, 12) + '...';
+        }
+        nameArr.push(name);
+        if (nameArr.length >= 2) {
+          break;
+        }
+      }
+    }
+    str += nameArr.join('、');
+    if (nameArr.length > 2) {
+      str += ' 等';
+    }
+    str += ' 缺货';
+    wx.showModal({
+      title: '下单失败',
+      content: str,
+      showCancel: false,
+      success: function(res) {
+
+      }
+    });
+  },
+  /*
+   *开始支付
+   * params:
+   * id - {int}订单id
+   */
+  _execPay: function(id) {
+    // if (!order.onPay) {
+    //   this.showTips('支付提示', '本产品仅用于演示，支付系统已屏蔽', true); //屏蔽支付，提示
+    //   this.deleteProducts(); //将已经下单的商品从购物车删除
+    //   return;
+    // }
+    var that = this;
+    order.execPay(id, (statusCode) => {
+      if (statusCode != 0) {
+        that.deleteProducts(); //将已经下单的商品从购物车删除   当状态为0时，表示
+
+        var flag = statusCode == 2;
+        wx.navigateTo({
+          url: '../pay-result/pay-result?id=' + id + '&flag=' + flag + '&from=order'
+        });
+      }
+    });
+  },
+  //将已经下单的商品从购物车删除
+  deleteProducts: function() {
+    var ids = [],
+      arr = this.data.productsArr;
+    for (let i = 0; i < arr.length; i++) {
+      ids.push(arr[i].id);
+    }
+    cart.delete(ids);
+  },
 })
